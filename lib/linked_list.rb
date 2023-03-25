93c27e6002343c5598fb7b12ac959667b791a4d3
Matthew Lim <matlim200@gmail.com>
2023-03-25 14:56:13 -0600

    add append method and append test

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,9 +1,26 @@
 class LinkedList
-  attr_reader :head 
+  attr_reader :head
   def initialize
     @head = nil
   end
 
+    # takes in a value check if linkedlist is empty(nil) 
+    # then sets head to new node and store new value in node
   def append(data)
-    node = Node.new(data)
+    new_node = Node.new(data)
+    if @head.nil?
+      @head = new_node
+    else
+      current_node = @head
+      while current_node.next_node != nil
+        current_node = current_node.next_node
+      end
+      current_node.next_node = new_node
+    end
+      #if there is already a head create new node at end of first node
+    def
   end
+
+
+ end
+
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -7,6 +7,21 @@ RSpec.describe LinkedList do
   it 'exists' do
     list = LinkedList.new
     expect(list).to be_an_instance_of(LinkedList)
+    expect(list.head).to eq(nil)
+  end
+    
+  it 'has append' do
+    list = LinkedList.new
+    list.append("doop")
+    expect(list.head.data).to eq("doop")
+    expect(@next_node).to eq(nil)
   end
 
+  it 'has count method' do
+    list = LinkedList.new
+    expect(list.count).to eq(1)
+  end
 end
+
+  # before:(each) do @list = LinkedList.new 
+  # end
